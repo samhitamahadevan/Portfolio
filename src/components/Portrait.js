75fd68cc786fca3dae35d8fe4af44a45095aa93c
Abdul Rauf <rauf11.abdul@gmail.com>
2024-09-07 19:10:56 +0500

    added image

--- src/components/Portrait.js
+++ src/components/Portrait.js
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 export default function Portrait() {
   return (
-    <div className='relative overflow-hidden box col-span-3 row-span-4 max-lg:col-span-4 max-md:col-span-12'>
+    <div className='bg-secondary relative overflow-hidden box col-span-3 row-span-4 max-lg:col-span-4 max-md:col-span-12'>
       {/* <div className='content'>Image</div> */}
       <Image
         src='/portrait.png'
         fill={true}
         alt='Picture of the author'
-        className='block object-cover object-top'
+        className='block object-contain object-right-bottom'
       />
     </div>
   );
