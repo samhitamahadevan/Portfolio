22d4c5cd609c584564dd6c2902ad765f56e6f597
samhitamahadevan <69466761+samhitamahadevan@users.noreply.github.com>
2025-04-23 19:37:26 -0400

    Spring theme

--- src/components/About.js
+++ src/components/About.js
@@ -37,7 +37,7 @@ export default function About({ data, timeline }) {
 
         <p className='about-text max-w-[28rem] pb-2 text-base leading-[135%]'>
           {/* {data?.text} */}
-          cs master's candidate at purdue (2025) with experience as a graduate research assistant in applying foundational models to digital forensics. my technical journey, rooted at anna university, includes roles at {' '}
+          cs master&apos;s candidate at purdue (2025) with experience as a graduate research assistant in applying foundational models to digital forensics. my technical journey, rooted at anna university, includes roles at {' '}
         <a href="https://www.striim.com" target="_blank" rel="noopener noreferrer" className="underline">
           striim Inc.
         </a>,{' '}
@@ -46,7 +46,7 @@ export default function About({ data, timeline }) {
         </a>, and{' '}
         <a href="https://cardeahealth.io" target="_blank" rel="noopener noreferrer" className="underline">
           cardea health
-        </a>,providing diverse experience across technology domains. i'm passionate about transforming concepts into reality, delivering impactful ai solutions that solve real problems. beyond tech, i find balance through swimming and trivia games. always eager to collaborate on innovative ml/ai projects that push boundaries and satisfy my natural curiosity.
+        </a>, providing diverse experience across technology domains. i&apos;m passionate about transforming concepts into reality, delivering impactful ai solutions that solve real problems. beyond tech, i find balance through swimming and trivia games. always eager to collaborate on innovative ml/ai projects that push boundaries and satisfy my natural curiosity.
         </p>
       </div>
     </Box>
